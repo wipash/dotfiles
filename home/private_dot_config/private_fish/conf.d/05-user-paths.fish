009b0f6aec2d2b45ccbf9c4281ec758455b1fa61
Sean McGrath <sean@mcgrath.net.nz>
2026-08-28 11:24:58 +1200

    fix some path stuff

---
+++ home/private_dot_config/private_fish/conf.d/05-user-paths.fish
@@ -0,0 +1,12 @@
+fish_add_path -ga $HOME/.bun/bin
+fish_add_path -ga $HOME/.dotnet
+fish_add_path -ga $HOME/.dotnet/tools
+fish_add_path -ga $HOME/.poetry/bin
+fish_add_path -ga $HOME/.fzf/bin
+
+fish_add_path -ga $HOME/.local/bin
+
+fish_add_path -ga $HOME/.local/share/aquaproj-aqua/bin
+fish_add_path -ga /home/linuxbrew/.linuxbrew/opt/fzf/bin
+fish_add_path -ga "/mnt/c/Program Files/Docker/Docker/resources/bin"
+fish_add_path -ga $HOME/bin
